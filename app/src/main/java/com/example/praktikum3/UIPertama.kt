689bdfb511620b3839a0b62bab1f7606b6095f05
rnktlhr <rinakitluhur10@gmail.com>
2025-10-23 12:10:38 +0700

    menambahkan color pada text alamat di dalam column di fun aktivitas

--- app/src/main/java/com/example/praktikum3/UIPertama.kt
+++ app/src/main/java/com/example/praktikum3/UIPertama.kt
@@ -61,7 +61,7 @@ fun ActivitasPertama(modifier: Modifier) {
                     Text(
                         stringResource(R.string.alamat),
                         fontSize = 20.sp,
-
+                        color = Color.Yellow
                     )
                 }
             }

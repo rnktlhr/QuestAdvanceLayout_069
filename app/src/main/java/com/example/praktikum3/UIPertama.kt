96572d00a0b73f4e42c76351084057965d5a467b
rnktlhr <rinakitluhur10@gmail.com>
2025-10-23 11:24:03 +0700

    menambahakan source di strings.xml

---
+++ app/src/main/java/com/example/praktikum3/UIPertama.kt
@@ -0,0 +1,17 @@
+package com.example.praktikum3
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@@Composable
+fun ActivitasPertama(modifier: Modifier = Modifier) {
+    Column (modifier = Modifier.padding(top=100.dp)
+        .fillMaxSize()
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+}
+

ac2a15a4dc4fff8ac60b04e69930ca31cf5e97c3
rnktlhr <rinakitluhur10@gmail.com>
2025-10-23 11:37:07 +0700

    menambahkan column dan text di dalam column di fun aktivitas

--- app/src/main/java/com/example/praktikum3/UIPertama.kt
+++ app/src/main/java/com/example/praktikum3/UIPertama.kt
@@ -3,15 +3,26 @@ package com.example.praktikum3
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
-@@Composable
-fun ActivitasPertama(modifier: Modifier = Modifier) {
-    Column (modifier = Modifier.padding(top=100.dp)
-        .fillMaxSize()
+@Composable
+fun ActivitasPertama(modifier: Modifier) {
+    Column(modifier = Modifier.padding(top=100.dp)
+        .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
-    )
+    ){
+        Text(text= stringResource(id=R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+    }
 }
 

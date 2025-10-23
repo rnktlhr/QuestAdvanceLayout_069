0193ec567117b9d23abc77c0bb54dea95035b906
rnktlhr <rinakitluhur10@gmail.com>
2025-10-23 11:58:11 +0700

    menambahkan image di dalam row di fun aktivitas

--- app/src/main/java/com/example/praktikum3/UIPertama.kt
+++ app/src/main/java/com/example/praktikum3/UIPertama.kt
@@ -1,11 +1,14 @@
 package com.example.praktikum3
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -13,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -36,7 +40,15 @@ fun ActivitasPertama(modifier: Modifier) {
             colors = CardDefaults.cardColors(
                 containerColor = Color.LightGray
             )){
+            Row(){
+                val gambar = painterResource(R.drawable.Logo)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier.size(100.dp).padding(5.dp)
+                )
 
+            }
         }
     }
 }

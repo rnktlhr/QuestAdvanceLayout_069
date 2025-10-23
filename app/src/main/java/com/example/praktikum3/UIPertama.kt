592d52171b9b562c4e6a5030c5f28e0032a8fdd2
rnktlhr <rinakitluhur10@gmail.com>
2025-10-23 11:52:01 +0700

    menambahkan card color di dalam column di fun aktivitas

--- app/src/main/java/com/example/praktikum3/UIPertama.kt
+++ app/src/main/java/com/example/praktikum3/UIPertama.kt
@@ -3,12 +3,16 @@ package com.example.praktikum3
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -26,6 +30,14 @@ fun ActivitasPertama(modifier: Modifier) {
         Text(stringResource(R.string.univ),
             fontSize = 22.sp)
         Spacer(modifier = Modifier.height(25.dp))
+        Card( modifier = Modifier
+            .fillMaxWidth(1f)
+            .padding(12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.LightGray
+            )){
+
+        }
     }
 }
 
